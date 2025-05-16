fb76128d7e7690844d2c83bfc5fc699d14a997c2
01060c <xxs239239@gmail.com>
2025-05-16 17:46:02 +0530

    Create io.cpp

---
+++ io.cpp
@@ -0,0 +1,15 @@
+#include "io.hpp"
+
+IO::IO(connection_hdl &x) : hdl(x), sentPing(false), sentHello(false), locked(false), screen_width(0), screen_height(0), memberId(0), roomId(0), ip("") {}
+
+void IO::lock() {
+    locked = true;
+}
+
+void IO::unlock() {
+    locked = false;
+}
+
+bool IO::isInGame() {
+    return roomId > 0 && memberId > 0;
+}
