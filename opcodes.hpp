f21b02f22b9d99a489cd9ed605cd657100b14ed8
01060c <xxs239239@gmail.com>
2025-05-16 20:52:31 +0530

    Update opcodes.hpp

--- opcodes.hpp
+++ opcodes.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 
+// Opcodes
+// Client -> Server
 constexpr uint8_t OPCODE_CS_PING = 0x00;
 constexpr uint8_t OPCODE_CS_PONG = 0x01;
 constexpr uint8_t OPCODE_DIMENSIONS = 0x02;
@@ -14,6 +16,7 @@ constexpr uint8_t OPCODE_NICK = 0x07;
 constexpr uint8_t OPCODE_COLOR = 0x08;
 constexpr uint8_t OPCODE_DISPATCH = 0x09;
 
+// Server -> Client
 constexpr uint8_t OPCODE_SC_PING = 0x00;
 constexpr uint8_t OPCODE_SC_PONG = 0x01;
 constexpr uint8_t OPCODE_CONFIG = 0xA0;
@@ -21,10 +24,18 @@ constexpr uint8_t OPCODE_ENTERED_GAME = 0xA1;
 constexpr uint8_t OPCODE_INFO = 0xB0;
 constexpr uint8_t OPCODE_EVENTS = 0xA2;
 
+// Flags
+// Server -> Client
 constexpr uint8_t FLAG_CURSOR = 0x11;
 constexpr uint8_t FLAG_DIRECTORY = 0x12;
 constexpr uint8_t FLAG_FILE = 0x13;
 
+// Events
+// Client -> Server
+constexpr uint8_t DISPATCH_CHANGE_DIRECTORY = 0x01;
+constexpr uint8_t DISPATCH_LIST_DIRECTORY = 0x02;
+
+// Server -> Client
 constexpr uint8_t EVENT_CURSOR_ADD = 0x00;
 constexpr uint8_t EVENT_CURSOR_DELETE = 0x01;
 constexpr uint8_t EVENT_CHANGE_DIRECTORY = 0x02;
